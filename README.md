80160a5d462f902614eb26074e91f12c7c1b68ab
Tomas <tomasesantacruz@gmail.com>
2022-11-30 10:45:58 -0300

    First commit. Only work in desktop

---
+++ README.md
@@ -0,0 +1,7 @@
+# The dog API
+
+\*\*
+
+## Description
+
+This is a simple Vanilla JS project that consume this <a href="https://thedogapi.com/">API</a> using fetch. You can view the project <a href="">here</a>
---
+++ src/index.js
@@ -0,0 +1,19 @@
+async function Img() {
+  const imgContainer = document.querySelector(".container");
+  const limit = document.getElementsByName("input");
+  const API_URL = `https://api.thedogapi.com/v1/images/search?limit=${limit[0].value}&api_key=live_c6lMhShvb9iIc1o1FDZZf51xDv6p0rfqrk5i0fkKIjKBOoRqFc81miJBsnVqMGBp`;
+
+  imgContainer.replaceChildren(
+    document.createRange().createContextualFragment(``)
+  );
+
+  const res = await fetch(API_URL);
+  const data = await res.json();
+  for (let i = 0; i < limit[0].value; i++) {
+    const img = document
+      .createRange()
+      .createContextualFragment(`<img src=${data[i].url} alt=""/>`);
+    console.log(img);
+    imgContainer.append(img);
+  }
+}
